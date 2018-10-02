2065f45a14cbdb1f8ce16c66aa16d277d13458d9
ManuelSanchez25 <39495445+ManuelSanchez25@users.noreply.github.com>
2018-10-01 21:53:09 -0500

    Add files via upload
    primera parte :)

--- src/main/java/mx/iteso/User.java
+++ src/main/java/mx/iteso/User.java
@@ -1,5 +1,33 @@
 package mx.iteso;
 
+/**
+* clase usuario.
+ * */
+
 public abstract class User {
+
+    /**
+     * @return si el usuario puede no publicar.
+     */
+    public abstract boolean publish();
+
+    /**
+     * @return si el usuario puede  comentar.
+     */
+    public abstract boolean comment();
+
+    /**
+     * @return si el usuario puede aprobar.
+     */
+    public abstract boolean approve();
+
+    /**
+     * @return tsi el usuario puede  eliminar comentarios.
+     * *
+     */
+    public abstract boolean delete();
+
+
+
 
 }
---
+++ src/main/java/mx/iteso/usuariobase.java
@@ -0,0 +1,36 @@
+package mx.iteso;
+
+
+/**
+*usuairio base.
+  */
+public class usuariobase extends User {
+
+    /**
+     * @return si el usuario puede o no publicar
+     */
+    public final boolean publish() {
+        return false;
+    }
+
+    /**
+     * @return si el usuario puede o no comentar.
+     */
+    public final boolean comment() {
+        return false;
+    }
+
+    /**
+     * @return si el usuario puede o no eliminar comentarios.
+     */
+    public final boolean delete() {
+        return false;
+    }
+
+    /**
+     * @return si el usuario puede o no aprobar.
+     */
+    public final boolean approve() {
+        return false;
+    }
+}
